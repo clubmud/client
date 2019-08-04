db4e9e3014df8b0515f4e02d175f607c4d129d07
scipio <tactnician@gmail.com>
2019-08-03 23:29:22 -0400

    naive implementation of jwt auth on clinet

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Nav = (props) => {
+
+    const logged_out_nav = (
+        <ul>
+          <li onClick={() => props.display_form('login')}>login</li>
+          <li onClick={() => props.display_form('signup')}>signup</li>
+        </ul>
+      )
+
+      const logged_in_nav = (
+        <ul>
+          <li onClick={props.handle_logout}>logout</li>
+        </ul>
+      );
+
+      return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
+}
+
+export default Nav
